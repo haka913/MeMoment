e1dd15cbb4f804d5aec30a679ba1e0d5e5ce87b9
haka913 <43568065+haka913@users.noreply.github.com>
2021-01-25 23:27:19 +0900

    초기 skeleton 구성 기초적인 recyclerview, realm 구성 (TODO: memoDB attr 추가, 메모 수정, 삭제 기능 구현, view에 text 외 attrs 추가, 최신 순으로 sort)

---
+++ app/src/main/java/com/haka/memoment/MemoAdapter.kt
@@ -0,0 +1,34 @@
+package com.haka.memoment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.RealmResults
+import kotlinx.android.synthetic.main.memo_recycler_layout.view.*
+
+class MemoAdapter(private val context:Context?, private val memoList: RealmResults<MemoDB>):RecyclerView.Adapter<MemoAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.memo_recycler_layout, parent, false)
+
+//        return RecyclerView.ViewHolder(view)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return memoList.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.itemView.memoTextRV.text = memoList[position]!!.text
+        // TODO id, date, image, gps 추가
+    }
+
+    inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val textMemo = itemView.findViewById<TextView>(R.id.memoTextRV)
+        // TODO id, date, image, gps 추가
+    }
+}
+
