68ee53f5ecfece7af77f07815951441354e1a5a8
haka913 <43568065+haka913@users.noreply.github.com>
2021-02-18 16:42:55 +0900

    Geocoder로 위경도 주소로 변환 (MemoDetailActivity에 구현) TODO: googlemaps에 해당 메모들 marker로 찍기

--- app/src/main/java/com/haka/memoment/MemoDetailActivity.java
+++ app/src/main/java/com/haka/memoment/MemoDetailActivity.java
@@ -1,13 +1,18 @@
 package com.haka.memoment;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
+
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MemoDetailActivity extends AppCompatActivity {
 
@@ -24,20 +29,40 @@ public class MemoDetailActivity extends AppCompatActivity {
         String stringDate = intent.getStringExtra("date");
         String stringLat = intent.getStringExtra("latitude");
         String stringLng = intent.getStringExtra("longitude");
+        final Geocoder geocoder = new Geocoder(this);
+        List<Address> addr = null;
+        double latitude = Double.parseDouble(stringLat);
+        double longitude = Double.parseDouble(stringLng);
+        try {
+            addr = geocoder.getFromLocation(latitude, longitude, 10);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("addr error", "$e");
+        }
 
 
         TextView textView1 = (TextView) findViewById(R.id.text1);
         TextView textView2 = (TextView) findViewById(R.id.text2);
         TextView txLat = (TextView) findViewById(R.id.txLat);
         TextView txLng = (TextView) findViewById(R.id.txLng);
+        TextView txAddr = (TextView) findViewById(R.id.txAddr);
         textView1.setText(stringText);
         textView2.setText(stringDate);
         txLat.setText(stringLat);
         txLng.setText(stringLng);
+        String geoToAddr = "";
+        if (addr != null) {
+            if (addr.size() == 0) {
+                txAddr.setText("해당 주소 정보가 없습니다.");
+            }else{
+                geoToAddr = addr.get(0).getAddressLine(0).toString();
+//                txAddr.setText(addr.get(0).toString());
+                txAddr.setText(geoToAddr);
+            }
+        }
 
         //textView1.setText("text");
         //textView2.setText("date");
-
 
 
     }
